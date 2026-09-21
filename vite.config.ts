ff19c049ec26423fe87c2565a23781a772c5e950
Maryna Dubchak <maryna.dubchak@gmail.com>
2026-09-21 22:11:03 +0300

    Deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  base: '/3R/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
